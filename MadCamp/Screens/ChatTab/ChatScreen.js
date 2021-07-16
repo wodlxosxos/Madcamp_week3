5fd72e8b3821d441380c92e364817a2825eb0332
Seung-an-Jung <qnwktmddks@gmail.com>
2021-07-16 22:17:18 +0900

    styled-components yet

--- MadCamp/Screens/ChatTab/ChatScreen.js
+++ MadCamp/Screens/ChatTab/ChatScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import Rental from './screens/rental';
+import Deal from './screens/deal';
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function ChatScreen({route, navigation}) {
   return (
-    <View>
-      <Text>chat</Text>
-    </View>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'white',
+        indicatorStyle: {backgroundColor: 'white'},
+        style: {backgroundColor: '#0C579F'},
+      }}>
+      <Tab.Screen name="대여" component={Rental} />
+      <Tab.Screen name="거래" component={Deal} />
+    </Tab.Navigator>
   );
 }
